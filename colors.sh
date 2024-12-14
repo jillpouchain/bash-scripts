7242eee80e04a18c278c4bcd5fa35e0e837ab302
Jill <jillpouchain@gmail.com>
2024-12-14 03:29:27 +0100

    Updated colors

--- colors.sh
+++ colors.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# --Reset--
+# Reset
 RESET='\e[0m'
 
-# --Colors--
+# Colors
 BLACK='\e[0;30m'
 BLUE='\e[0;34m'
 CYAN='\e[0;36m'
@@ -13,7 +13,7 @@ RED='\e[0;31m'
 WHITE='\e[0;37m'
 YELLOW='\e[0;33m'
 
-# --Background--
+# Background
 BG_BLACK='\e[40m'
 BG_BLUE='\e[44m'
 BG_CYAN='\e[46m'
@@ -23,7 +23,7 @@ BG_RED='\e[41m'
 BG_WHITE='\e[47m'
 BG_YELLOW='\e[43m'
 
-# --Bold--
+# Bold
 B_BLACK='\e[1;30m'
 B_BLUE='\e[1;34m'
 B_CYAN='\e[1;36m'
@@ -33,7 +33,7 @@ B_RED='\e[1;31m'
 B_WHITE='\e[1;37m'
 B_YELLOW='\e[1;33m'
 
-# --Underline--
+# Underline
 U_BLACK='\e[4;30m'
 U_BLUE='\e[4;34m'
 U_CYAN='\e[4;36m'
@@ -43,6 +43,7 @@ U_RED='\e[4;31m'
 U_WHITE='\e[4;37m'
 U_YELLOW='\e[4;33m'
 
-# --Miscellaneous--
+# Miscellaneous
 BLACK_AND_WHITE='\e[1;30;47m' # Black text, white background
 BLINKING_RED_BACKGROUND='\001\033[5;41m\002'; # White text, red blinking background
+DIM_WHITE='\e[2;37m' # Dim white text
